fef8a9cd7c531fa217d95ab686adfc92b51e6814
JeongYongCheol <dydcjf611@gmail.com>
2023-02-28 00:54:48 +0900

    Initial commit

---
+++ src/main/java/com/example/cnuorder/CnuOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.cnuorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnuOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnuOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cnuorder/CnuOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cnuorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnuOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
