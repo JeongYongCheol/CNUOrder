111aa4db50f7d218c9e00823be1c381627690546
JeongYongCheol <dydcjf611@gmail.com>
2023-02-28 01:00:41 +0900

    entity Product, enum Category 구현

---
+++ src/main/java/com/example/cnuorder/entity/Category.java
@@ -0,0 +1,5 @@
+package com.example.cnuorder.entity;
+
+public enum Category {
+    COFFEE_BEAN_PACKAGE
+}
---
+++ src/main/java/com/example/cnuorder/entity/Product.java
@@ -0,0 +1,59 @@
+package com.example.cnuorder.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    private UUID productId;
+    private String productName;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    private long price;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Product(UUID productId, String productName, Category category, long price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
